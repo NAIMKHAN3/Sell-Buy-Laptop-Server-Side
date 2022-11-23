001299461039fdbb51fc460594bceb3b87e3aef7
NAIMKHAN3 <naimkhan01856@gmail.com>
2022-11-24 05:11:33 +0600

    create a user database

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 const app = express();
+const port = process.env.PORT || 5000;
+
+
 
 app.use(cors());
 app.use(express.json());
@@ -11,6 +14,37 @@ app.get('/', (req, res) => {
     res.send({ status: true, Message: 'Sell Buy Laptop Server Is Running' })
 })
 
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ujhfrio.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const userCollection = client.db("sell-buy-laptop").collection("user")
+
+        app.post('/user', async (req, res) => {
+            try {
+                const user = req.body;
+                const result = await userCollection.insertOne(user);
+                res.send(result)
+
+            }
+            catch {
+                res.send({ status: false, message: "cannot insert user" })
+            }
+
+        })
+
+
+
+
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
 
 
 app.listen(port, () => {
